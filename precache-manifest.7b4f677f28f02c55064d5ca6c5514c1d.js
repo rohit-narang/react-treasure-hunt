1dbecb179b97fb71ad1994ca60bfa3c7f7dc3b04
Rohit Narang <rohitnarang12@gmail.com>
2019-08-12 01:48:56 +0530

    Updates

---
+++ precache-manifest.7b4f677f28f02c55064d5ca6c5514c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4456fa40090812c3af57084502b6633",
+    "url": "/react-treasure-hunt/index.html"
+  },
+  {
+    "revision": "6bffb1598cef756a3934",
+    "url": "/react-treasure-hunt/static/css/main.b4aed523.chunk.css"
+  },
+  {
+    "revision": "0a1e1bddee4f6f698361",
+    "url": "/react-treasure-hunt/static/js/2.cae55553.chunk.js"
+  },
+  {
+    "revision": "6bffb1598cef756a3934",
+    "url": "/react-treasure-hunt/static/js/main.d748935d.chunk.js"
+  },
+  {
+    "revision": "11c535179797efd8f5ce",
+    "url": "/react-treasure-hunt/static/js/runtime~main.818db641.js"
+  },
+  {
+    "revision": "3a1dda06eaeb07a81ed3faee3d3b1edf",
+    "url": "/react-treasure-hunt/static/media/myplayer.3a1dda06.png"
+  }
+]);
